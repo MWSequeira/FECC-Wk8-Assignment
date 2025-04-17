94fb4489148a5694c292e529091af423994bc5b0
Michele W Sequeira <miche@alum.mit.edu>
2025-04-17 15:09:11 -0600

    first commit

---
+++ week8-assign.js
@@ -0,0 +1,32 @@
+/*
+Coding Steps:
+Create a menu app as seen in this week’s video. What you create is up to you as long as it meets the following requirements:
+1. Use at least one array.
+2. Use at least two classes.
+3. Your menu should have the options to create, view, and delete elements.
+ 
+
+Video Steps:
+Create a video, up to five minutes max, showing and explaining how your project works with an emphasis on the portions you contributed.
+* This video should be done using screen share and voice over.
+* This can easily be done using Zoom, although you don't have to use Zoom, it's just what we recommend.
+* You can create a new meeting, start screen sharing, and start recording.
+* This will create a video recording on your computer.
+* This should then be uploaded to a publicly accessible site, such as YouTube.
+* Ensure the link you share is PUBLIC or UNLISTED!
+
+Coding Assignment Submission
+This assignment is designed as a learning exercise to help you build and apply key skills.
+*** You do not need to submit it*** but we encourage you to complete the steps to reinforce your understanding. Focus on practicing, experimenting, and improving your work to get the most out of this activity.
+
+*/
+
+class Player (name, phone, position) {
+    this.name = name;
+    this.phone = phone;
+    this.position = position;
+}
+
+class Team (teamName) {
+    this.teamName = teamName;
+}
