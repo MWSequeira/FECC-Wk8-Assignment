5cfa680b768a732c8422888420c322353c086df4
Michele W Sequeira <miche@alum.mit.edu>
2025-04-17 20:47:03 -0600

    Creating a menu
    using demo as a starting point

--- week8-assign.js
+++ week8-assign.js
@@ -21,12 +21,150 @@ This assignment is designed as a learning exercise to help you build and apply k
 
 */
 
-class Player (name, phone, position) {
-    this.name = name;
-    this.phone = phone;
-    this.position = position;
+class Player {
+    constructor (name, phone, position) {
+        this.name = name;
+        this.phone = phone;
+        this.position = position;
+    }
+    
+    addPlayer(){ //adds a player to the league
+        this.name = prompt('Player Name: ');
+        this.phone = prompt('Phone number: ');
+        this.position = prompt('Position: ');
+        return [this.name, this.phone, this.position];
+    }
+
+    display() { //displays player info
+        return `${this.name}, ${this.phone}, plays ${this.position}.`
+    }
 }
 
-class Team (teamName) {
-    this.teamName = teamName;
+
+class Team {
+    constructor (teamName) {
+        this.teamName = teamName;
+    }
+    
+    createTeam (teamName) { //names team, creates the team array
+        this.teamName = teamName;
+        this.teamRoster = [];
+        // doesn't return anything yet
+    }
+
+    addPlayer (player){
+        if (player instanceof Player) {
+            this.teamRoster.push(player);
+        } else {
+            throw new Error(`Player must be in the League.`);
+        }
+    }
+
+    displayNoPlayers (){
+        return `${this.teamName} has ${this.teamName.length} players.`
+    }
 }
+
+
+class Menu {
+    constructor() {
+        this.teams = [];
+        this.selectedTeam = null;
+    }
+
+    start(){
+        let selection = this.showMainMenuOptions();
+        while (selection !== 0) {
+            switch (selection) {
+                case '1':
+                    this.createTeam();
+                    break;
+                case '2':
+                    this.viewTeam();
+                    break;
+                case '3':
+                    this.deleteTeam();
+                    break;
+                case '4': 
+                    this.displayAllTeams();
+                    break;
+                default:
+                    selection = 0;
+
+            }
+            selection = this.showMainMenuOptions();
+        }
+        alert('Good-bye');
+    }
+
+    showMainMenuOptions() {
+        return prompt(`
+            0) Exit
+            1) Create new team
+            2) View a team
+            3) Delete a team
+            4) Display all teams
+            `);
+    }
+
+    showTeamMenuOptions(teamInfo){
+        return prompt(`
+            0) Go to previous menu
+            1) Add a player
+            2) Delete a player
+            -------------------
+            ${teamInfo}`);
+    }
+
+    createTeam() {
+        let teamName = prompt('Enter Name of New Team: ');
+        this.teams.push(new Team(teamName));
+    }
+
+    viewTeam () {
+        let index = prompt('Enter number of team to view: ');
+        if (index > -1 && index < this.teams.length) {
+            this.selectedTeam = this.teams[index];
+            let description = ("Team Name: " + this.selectedTeam + "\n");
+
+            for (let i = 0; i < this.selectedTeam.players.length; i++) {
+                roster += i + ") " + this.selectedTeam.players[i].name + ", " + this.selectedTeam.players[i].position + "\n";
+            }
+        let selection = this.showTeamMenuOptions(description);
+        switch (selection) {
+            case '1':
+                this.createPlayer();
+                break;
+            case '2':
+                this.deletePlayer();
+                break;
+        }
+        }
+    }
+
+
+    deleteTeam () {
+
+    }
+
+    displayAllTeams () {
+        let teamString = "";
+        for (let i = 0; i < this.teams.length; i++) {
+            teamString += i + ") " + this.teams[i].teamName + "\n";
+        }
+        alert(teamString);
+    }
+}
+
+// Test Player class- create and display players
+let player1 = new Player ("Mike", "505-259-9004", "Defense");
+console.log (player1.display());
+
+let player2 = new Player ("Zach", "505-505-5050", "Forward");
+console.log (player2.display());
+
+let player3 = new Player ("Seth", "505-555-5555", "Goalie");
+console.log (player3.display());
+
+let menu = new Menu();
+menu.start();
